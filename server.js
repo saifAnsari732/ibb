22de48c31c559ccf78592c174c60f41226175f13
saifAnsari732 <ansarisaifuddin732@gmail.com>
2026-01-30 15:43:51 +0530

    fir44it

--- server.js
+++ server.js
@@ -395,7 +395,7 @@ app.get('/api', (req, res) => {
 });
  
 // Start Serve
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
